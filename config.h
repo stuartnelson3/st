b56a4723b79d9f8b7dfb5c36e9d205a6408813fb
stuart nelson <stuartnelson3@gmail.com>
2019-01-14 10:08:01 +0100

    Allow bold/normal font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Consolas:style=Bold:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Consolas:pixelsize=16";
 static int borderpx = 2;
 
 /*
